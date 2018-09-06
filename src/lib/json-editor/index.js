9e1286af14c9d49b02929ec730f2e5585632e96b
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2018-09-06 13:13:48 +0300

    push new changes

--- src/lib/json-editor/index.js
+++ src/lib/json-editor/index.js
@@ -1,27 +1,6 @@
 import React from 'react';
 import ReactJson from 'react-json-view';
 
-const theme = {
-  scheme: 'monokai',
-  author: 'wimer hazenberg (http://www.monokai.nl)',
-  base00: '#272822',
-  base01: '#383830',
-  base02: '#49483e',
-  base03: '#75715e',
-  base04: '#a59f85',
-  base05: '#f8f8f2',
-  base06: '#f5f4f1',
-  base07: '#f9f8f5',
-  base08: '#f92672',
-  base09: '#fd971f',
-  base0A: '#f4bf75',
-  base0B: '#a6e22e',
-  base0C: '#a1efe4',
-  base0D: '#66d9ef',
-  base0E: '#ae81ff',
-  base0F: '#cc6633'
-};
-
 const data = {
   array: [1, 2, 3],
   bool: true,
@@ -31,16 +10,58 @@ const data = {
   immutable: { key: 'value' }
 };
 
-const JSONEditor = () => <div>
-  <ReactJson src={data} name={false} collapsed={false} shouldCollapse={(field) => {
-    if (!field.name) {
-      return false;
-    }
-    
-    console.log(field);
-    
+const shouldCollapse = (field) => {
+  if (!field.name) {
     return true;
-  }}/>
+  }
+  return false;
+};
+
+export const theme = {
+  scheme: 'custom-theme',
+  base00: '#3F3F3F',
+  base01: '#DEDEDE',
+  base02: '#57D7FF',
+  base03: '#DEDEDE',
+  base04: '#DEDEDE',
+  base05: '#DEDEDE',
+  base06: '#DEDEDE',
+  base07: '#57D7FF',
+  base08: '#DEDEDE',
+  base09: '#DEDEDE',
+  base0A: '#DEDEDE',
+  base0B: '#DEDEDE',
+  base0C: '#57D7FF',
+  base0D: '#DEDEDE',
+  base0E: '#DEDEDE',
+  base0F: '#DEDEDE'
+};
+
+const CustomEditor = () => <ReactJson src={data}
+                                      name={false}
+                                      shouldCollapse={shouldCollapse}
+                                      iconStyle={'circle'}
+                                      theme={theme}
+                                      displayDataTypes={false}
+                                      displayObjectSize={false}
+                                      enableClipboard={true}
+                                      onDelete={true}
+                                      onEdit={true}/>;
+                                      
+const Row = () => <div style={{display: 'flex', flexDirection: 'row'}}>
+  <div style={{width: '50px'}}>2</div>
+  <div style={{width: '100px'}}>15:05:06</div>
+  <div style={{width: '350px'}}>
+    <CustomEditor/>
+  </div>
+</div>;
+
+const JSONEditor = () => <div style={{background: '#3F3F3F', width: '600px', height: '400px', color: '#767676'}}>
+  <div style={{padding: '12px'}}>
+    <Row/>
+    <Row/>
+  </div>
+  <div>X</div>
 </div>;
 
 export { JSONEditor };
