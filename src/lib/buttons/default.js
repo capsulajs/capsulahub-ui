5ed065a2ded2438622490b1e8d4363cabda03249
sergiupadure <sergiu.padure@jivygroup.com>
2018-09-12 14:32:43 +0300

    Fixes after code review

--- src/lib/buttons/default.js
+++ src/lib/buttons/default.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledButton = styled.button`
+const CButton = styled.button`
   font-size: 1em;
   font-family: Montserrat;
   font-weight: 500;
   text-align: center;
-  background-color: ${props => props.theme.props[0]};
-  color: ${props => props.theme.props[2]};
-  border: 1px solid ${props => props.theme.props[3]};
+  background-color: ${props => props.theme.bgColor};
+  color: ${props => props.theme.textColor};
+  border: 1px solid ${props => props.theme.borderColor};
   padding: 0.3em 1em;
   cursor: pointer;
 
   &:hover {
-    background-color: ${props => props.theme.props[1]};
+    background-color: ${props => props.theme.hoverBgColor};
   }
 
   ${props => props.css};
 `;
 
-//theme name: background color, hover background color, selected background color, text color, border
 const themes = [
-  {name: 'default', props: ['#57D7FF', '#4EBEE0', '#3B3B3B', '#57D7FF']},
-  {name: 'lightGrey', props: ['#EFEFEF', '#EFEFEF', '#3B3B3B', '#EFEFEF']},
-  {name: 'grey', props: ['#626262', '#626262', '#F7F7F7', '#626262']},
-  {name: 'dark', props: ['#3C3C3C', '#3C3C3C', '#57D7FF', '#57D7FF']},
-  {name: 'transparent', props: ['transparent', 'transparent', '#57D7FF', '#57D7FF']}
+  {name: 'default', props: {bgColor: '#57D7FF', hoverBgColor: '#4EBEE0', textColor: '#3B3B3B', borderColor: '#57D7FF'}},
+  {name: 'lightGrey', props: {bgColor: '#EFEFEF', hoverBgColor: '#EFEFEF', textColor: '#3B3B3B', borderColor: '#EFEFEF'}},
+  {name: 'grey', props: {bgColor: '#626262', hoverBgColor: '#626262', textColor: '#F7F7F7', borderColor: '#626262'}},
+  {name: 'dark', props: {bgColor: '#3C3C3C', hoverBgColor: '#3C3C3C', textColor: '#57D7FF', borderColor: '#57D7FF'}},
+  {name: 'transparent', props: {bgColor: '#transparent', hoverBgColor: '#transparent', textColor: '#57D7FF', borderColor: '#57D7FF'}}
 ];
 
 const Button = ({ text, theme, className, css, id, onClick}) => {
   let currentTheme = themes.find(x => x.name === theme) || themes.find(x => x.name === 'default');
-  return <StyledButton id={id} theme={currentTheme} className={className} css={css} onClick={onClick}>{text}</StyledButton>
+  return <CButton id={id} theme={currentTheme} className={className} css={css} onClick={onClick}>{text}</CButton>
 };
 
 export { Button };
