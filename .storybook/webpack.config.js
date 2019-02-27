a86a1ec623c50982f653c054e65c3f31e716bdb3
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-02-27 13:37:19 +0200

    commit it

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -4,32 +4,35 @@ const includePath = path.resolve(__dirname, '..');
 module.exports = {
   plugins: [],
   module: {
-    rules: [{
-      test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-      loaders: 'file-loader',
-      options: {
-        name: '[name].[hash].[ext]',
-        outputPath: 'assets/'
-      }
-    }, {
-      test: /\.(css|less)$/,
-      include: includePath,
-      use: [
-        {
-          loader: 'style-loader',
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+        loaders: 'file-loader',
+        options: {
+          name: '[name].[hash].[ext]',
+          outputPath: 'assets/',
         },
-        {
-          loader: 'css-loader',
-          options: {
-            sourceMap: true,
+      },
+      {
+        test: /\.(css|less)$/,
+        include: includePath,
+        use: [
+          {
+            loader: 'style-loader',
           },
-        },
-      ],
-    }]
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
     alias: {
-      src: path.resolve('./src')
-    }
-  }
+      src: path.resolve('./src'),
+    },
+  },
 };
