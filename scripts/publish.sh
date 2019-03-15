87f478539b3a073aa42ed004ca09076b8e322860
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-15 10:01:44 +0200

    update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -22,7 +22,7 @@ if [[ "$TRAVIS_BRANCH" =~ ^feature\/.*$ ]]; then
     else
         echo $MSG_VERSION_FAIL && exit 1
     fi
-    npm publish --tag snapshot --access public
+    npm publish --tag snapshot --access public --preid alpha
     if [[ "$?" == 0 ]]; then
         echo $MSG_PUBLISH_SUCCESS
     else
