3296b645da49183237c22c2e232007796344f7ab
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-20 10:53:20 +0200

    style fix

--- src/components/canvas/services/utils.js
+++ src/components/canvas/services/utils.js
@@ -16,6 +16,5 @@ export const getSectorCouple = (sectors, sector) => {
 };
 
 export const getDropzoneSectors = (dropzone) => (dropzone ? dropzone.sectors : []);
-export const subjectToObservable = (subject, events) => subject.asObservable().pipe(
-  filter(([event, metadata]) => events.includes(event))
-);
+export const subjectToObservable = (subject, events) =>
+  subject.asObservable().pipe(filter(([event, metadata]) => events.includes(event)));
