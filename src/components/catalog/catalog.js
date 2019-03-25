72d323953a8d05335714112da5683b990679cfd2
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-25 11:11:37 +0200

    done

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -27,13 +27,7 @@ export default class Catalog extends React.Component {
     return (
       <Container>
         {methods.map(({ name, children }) => (
-          <List
-            key={name}
-            name={name}
-            methods={children}
-            selectMethod={selectMethod}
-            selectedMethod={selectedMethod}
-          />
+          <List key={name} name={name} methods={children} selectMethod={selectMethod} selectedMethod={selectedMethod} />
         ))}
       </Container>
     );
--- src/components/catalog/utils.js
+++ src/components/catalog/utils.js
@@ -1,13 +1,13 @@
 import { flattenDeep } from 'lodash';
 
-const simplify = (methods) => methods.map(method => method.children ? simplify(method.children) : method);
+const simplify = (methods) => methods.map((method) => (method.children ? simplify(method.children) : method));
 
 export const isMethodsContain = (methods, selectedMethod) => {
   if (!selectedMethod) {
     return false;
   }
 
-  if (flattenDeep(simplify(methods)).find(method => method.id === selectedMethod.id)) {
+  if (flattenDeep(simplify(methods)).find((method) => method.id === selectedMethod.id)) {
     return true;
   }
 
