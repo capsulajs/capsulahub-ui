8aff43269dd4327aa7b43e4558bfdb51cab30d81
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-25 10:27:59 +0200

    init

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -27,7 +27,14 @@ export default class Catalog extends React.Component {
     return (
       <Container>
         {methods.map(({ name, children }, index) => (
-          <List key={name} index={index} name={name} methods={children} selectMethod={selectMethod} selectedMethod={selectedMethod}/>
+          <List
+            key={name}
+            index={index}
+            name={name}
+            methods={children}
+            selectMethod={selectMethod}
+            selectedMethod={selectedMethod}
+          />
         ))}
       </Container>
     );
