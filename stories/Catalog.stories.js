563d07894346bff0a8f675546597b4f42cb774b6
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-25 14:44:57 +0200

    refactoring

--- stories/Catalog.stories.js
+++ stories/Catalog.stories.js
@@ -9,85 +9,85 @@ const selectedMethod = {
 };
 
 const methods = [
-    {
-      id: 0,
-      name: 'Environment',
-      children: [
-        {
-          id: 92,
-          name: 'Product',
-          children: [
-            {
-              id: 929,
-              name: 'Service',
-              children: [
-                {
-                  id: 20,
-                  name: 'API 1',
-                },
-                {
-                  id: 24,
-                  name: 'API 2',
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 99,
-      name: 'Team',
-      children: [
-        {
-          id: 8,
-          name: 'Tim Drake',
-        },
-        {
-          id: 9,
-          name: 'Jason Todd',
-        },
-        {
-          id: 10,
-          name: 'Richard Grayson',
-          children: [
-            {
-              id: 8,
-              name: 'Tim Drake',
-            },
-          ],
-        },
-      ],
-    },
-    {
-      id: 11,
-      name: 'Services',
-      children: [
-        {
-          id: 12,
-          name: 'Web Development',
-        },
-        {
-          id: 13,
-          name: 'UI Design',
-        },
-        {
-          id: 7,
-          name: 'Copywriting',
-        },
-      ],
-    },
-  ];
+  {
+    id: 0,
+    name: 'Environment',
+    children: [
+      {
+        id: 92,
+        name: 'Product',
+        children: [
+          {
+            id: 929,
+            name: 'Service',
+            children: [
+              {
+                id: 20,
+                name: 'API 1',
+              },
+              {
+                id: 24,
+                name: 'API 2',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 99,
+    name: 'Team',
+    children: [
+      {
+        id: 8,
+        name: 'Tim Drake',
+      },
+      {
+        id: 9,
+        name: 'Jason Todd',
+      },
+      {
+        id: 10,
+        name: 'Richard Grayson',
+        children: [
+          {
+            id: 8,
+            name: 'Tim Drake',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: 'Services',
+    children: [
+      {
+        id: 12,
+        name: 'Web Development',
+      },
+      {
+        id: 13,
+        name: 'UI Design',
+      },
+      {
+        id: 7,
+        name: 'Copywriting',
+      },
+    ],
+  },
+];
 
 class CatalogExample extends React.Component {
   state = {
-    selectedMethod: methods[2].children[0]
+    selectedMethod: methods[2].children[0],
   };
 
-  selectMethod = (selectedMethod) => this.setState({ selectedMethod })
+  selectMethod = (selectedMethod) => this.setState({ selectedMethod });
 
   render() {
-    return <Catalog selectedMethod={this.state.selectedMethod} methods={methods} selectMethod={this.selectMethod}/>;
+    return <Catalog selectedMethod={this.state.selectedMethod} methods={methods} selectMethod={this.selectMethod} />;
   }
 }
 
