277f5c309ca7a5c8c77d741db5337b43b0c66dcc
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-03-29 11:12:12 +0200

    style fix

--- src/components/request-form/editor.js
+++ src/components/request-form/editor.js
@@ -25,26 +25,28 @@ export default class Editor extends React.Component {
 
   onChange = (newArgument) => {
     this.props.onChange(this.props.index, newArgument);
-  }
+  };
 
   render() {
     const { width, height, mode, onLoad, onChange, value } = this.props;
 
-    return <React.Fragment>
-      <AceEditor
-        mode={mode}
-        theme="capsula-js"
-        value={value}
-        onLoad={onLoad}
-        onChange={this.onChange}
-        fontSize={11}
-        setOptions={{
-          tabSize: 2,
-        }}
-        width={`${width}px`}
-        height={`${height}px`}
-      />
-      <Line />
-    </React.Fragment>
+    return (
+      <React.Fragment>
+        <AceEditor
+          mode={mode}
+          theme="capsula-js"
+          value={value}
+          onLoad={onLoad}
+          onChange={this.onChange}
+          fontSize={11}
+          setOptions={{
+            tabSize: 2,
+          }}
+          width={`${width}px`}
+          height={`${height}px`}
+        />
+        <Line />
+      </React.Fragment>
+    );
   }
 }
