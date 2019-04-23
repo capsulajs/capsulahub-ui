50056fa5553c1e56dab9b540f74c0ba6407531d3
Mykhailo Paliukh <paliukh.mykhailo@gmail.com>
2019-04-23 15:57:00 +0300

    style fix

--- stories/Canvas.stories.js
+++ stories/Canvas.stories.js
@@ -18,34 +18,27 @@ export default class CanvasExample extends React.Component {
         type: 'container',
         flex: 0.5,
         orientation: 'vertical',
-        nodes: [
-          {
-            id: 'node1',
-            type: 'element',
-            flex: 1,
-            tabs: [
-              {
-                id: 'tab1',
-                name: 'Tab 1',
-                content: '<web-cmponent-1></web-component-1>',
-              },
-            ],
-            activeTabIndex: 0,
-          },
-          {
-            id: 'node2',
-            type: 'element',
-            flex: 1,
-            tabs: [
-              {
-                id: 'tab2',
-                name: 'Tab 2',
-                content: '<web-cmponent-2></web-component-2>',
-              },
-            ],
-            activeTabIndex: 0,
-          },
-        ],
+        nodes: [{
+          id: 'node1',
+          type: 'element',
+          flex: 1,
+          tabs: [{
+            id: 'tab1',
+            name: 'Tab 1',
+            content: '<web-cmponent-1></web-component-1>'
+          }],
+          activeTabIndex: 0
+        }, {
+          id: 'node2',
+          type: 'element',
+          flex: 1,
+          tabs: [{
+            id: 'tab2',
+            name: 'Tab 2',
+            content: '<web-cmponent-2></web-component-2>'
+          }],
+          activeTabIndex: 0
+        }],
       },
     };
     this.onUpdate = this.onUpdate.bind(this);
@@ -64,4 +57,5 @@ export default class CanvasExample extends React.Component {
   }
 }
 
-storiesOf('Canvas', module).add('default', () => <CanvasExample />);
+storiesOf('Canvas', module)
+  .add('default', () => <CanvasExample />);
